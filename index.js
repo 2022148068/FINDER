575dec508cffb61589d8811ee587a3fd0bff524a
2022148068 <steve7161@yonsei.ac.kr>
2023-05-29 18:27:16 +0900

    5/29

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
-
+const request = require("request");
 const DB = new Map();
 const app = express();
+const sqlite3 = require("sqlite3");
+const sqlite = require("sqlite");
+
 
 app.use(express.static("public"));
-app.use(cookieParser());
+
+app.get("/tmp.html", function(req, res) {
+    res.sendFile(__dirname + "tmp.js");
+})
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/public/index.html");
 });
@@ -17,11 +23,7 @@ app.get("/loster.html", function(req, res) {
     res.sendFile(__dirname + "/public/loster.html");
 });
 
-
-
-
-
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, function() {
-    console.log("Server ready in port: " + PORT);
+    console.log("Server on. http://localhost:" + PORT);
 });
